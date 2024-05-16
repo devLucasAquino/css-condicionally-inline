e4562d70920f4379c1f5b895ca58c679cbe47be8
PauloBezz <paulo.bezerra2057@gmail.com>
2024-05-15 21:12:50 -0300

    init projecty

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './index.css';
+
+function App() {
+  return (
+    <div id="app">
+      <h1>CSS is great!</h1>
+      <menu>
+        <li>
+          <button>Yes</button>
+        </li>
+        <li>
+          <button>No</button>
+        </li>
+      </menu>
+    </div>
+
+  );
+}
+
+export default App;
